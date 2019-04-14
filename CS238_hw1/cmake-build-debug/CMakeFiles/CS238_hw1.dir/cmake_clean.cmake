334f44d02efbf175060d08fbcac9c22195185492
candr006 <candr006@ucr.edu>
2019-04-14 15:15:05 -0700

    add files

---
+++ CS238_hw1/cmake-build-debug/CMakeFiles/CS238_hw1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS238_hw1.dir/main.cpp.o"
+  "CS238_hw1.pdb"
+  "CS238_hw1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS238_hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CS238_hw1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void partialDigest(){
+
+    return;
+}
+
+int main(){
+
+    cout << "hello world";
+
+    return 0;
+}
