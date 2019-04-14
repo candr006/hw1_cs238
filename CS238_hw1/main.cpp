8b87da7a52a80a1dd588f4a9454f99e69b102c3b
candr006 <candr006@ucr.edu>
2019-04-14 16:08:31 -0700

    more functions

--- CS238_hw1/main.cpp
+++ CS238_hw1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> delta(int val, vector<int> v){
@@ -11,6 +12,20 @@ vector<int> delta(int val, vector<int> v){
     }
 
     return delta_vect;
+}
+
+bool containedWithin(vector<int> a, vector<int> b){
+    bool completelyContained=true;
+
+    for(int i=0; i<a.size(); i++){
+
+        //if a[i] is not in b, then a is not completely contained in b
+        if(b.end() == find(b.begin(), b.end(), a[i])){
+            completelyContained=false;
+        }
+    }
+
+    return completelyContained;
 }
 
 int getMaxIndex(vector<int> a){
@@ -37,7 +52,33 @@ vector<int> deleteVal(int a, vector<int> v){
     return new_v;
 }
 
+vector<int> deleteVectfromVect(vector<int> a, vector<int> b){
+    vector<int> new_v;
+    for(int i=0; i<b.size(); i++){
+        //if b[i] is not in a, then add to new vect
+        if(a.end() == find(a.begin(), a.end(), b[i])){
+            new_v.push_back(b[i]);
+        }
+    }
 
+    return new_v;
+}
+
+void printVect(vector<int> a, string message){
+    cout <<message << ": {";
+    for(int i=0; i<a.size(); i++){
+        cout<< a[i];
+
+        if(i!=(a.size()-1)) {
+            cout << ", ";
+        }
+    }
+    cout << "}" << endl;
+}
+
+void place(vector<int> l, vector<int> x){
+
+}
 
 void partialDigest(){
     vector<int> L = {0,1};
